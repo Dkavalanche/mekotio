3ef90b3585301130679bd2632458277ca10af208
Ernesto <ernesto.luis.bernal@gmail.com>
2023-06-30 18:35:24 -0300

    Add files via upload

---
+++ c2_mlw.py
@@ -0,0 +1,40 @@
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+
+    def handle(self):
+        self.data = self.request.recv(2048).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        self.request.sendall(b'<|WY|>')
+        #self.request.sendall(b'orca3whiid8gota@<|iuqm|>')
+        self.data = self.request.recv(2048).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        
+if __name__ == "__main__":
+    HOST, PORT =  "localhost", 6742
+
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+
+        server.serve_forever()
+
+#<|SNUNFHXQU|>
+#<|WY|> / Pregunta nombre de bot.
+#<|xywz|>1--09-06-N-96<|>32 - Windows 8.16.39600<|>TEMPO tempo123 windows defender-santandercomar<|>TEMPO tempo123<<|6//&M31R543V36m!santandercomar
+#<|stygqwokui|>8259478<<|<|iuqm|>
+#<|ywloz|><|dlpeaUoilF|>santander3.zip<<|67547<|iuqm|>
+#<|SLUT|><|Eoadivn|>###TEMPO
+#orca3whiid8gota@<|iuqm|>  bloquea
+#<|SLUT|>
+#iedabajamn<|yJuSakR|>86<|>104<<|<|uubkSaRA3yJ|>86<|>104<<|<|yJuSakR|>261<|>101<<|<|uubkSaRA3yJ|>261<|>101<<|<|yJuSakR|>1357<|>612<<|<|uubkSaRA3yJ|>1357<|>612<<|<|yJuSakR|>1359<|>135<<|<|uubkSaRA3yJ|>1359<|>134<<|<|iuqm|>
+#<|SLUT|>
+#<|yJuSakR|>41<|>101<<|<|uubkSaRA3yJ|>41<|>101<<|<|yJuSakR|>75<|>255<<|<|uubkSaRA3yJ|>75<|>255<<|<|yJuSakR|>722<|>293<<|<|uubkSaRA3yJ|>722<|>292<<|<|yJuSakR|>724<|>290<<|<|uubkSaRA3yJ|>724<|>290<<|<|yJuSakR|>263<|>455<<|<|uubkSaRA3yJ|>263<|>455<<|<|yJuSakR|>262<|>457<<|<|uubkSaRA3yJ|>262<|>457<<|<|iuqm|>
+#<|SLUT|>
+#cYvlbAkvltcYvlbAkvltcYvlbAkvltcYvlbAkvltcYvlbAkvltcYvlbAkvltcYvlbAkvlt<|iuqm|><|iuqm|><|iuqm|><|iuqm|>
+#1465B74AEA6EFC141ED755AAB9A9AAAB8EFB60AAACDD6EA78DF67F918A899CFB77EC67A3B2CC50B4D754C8B9C5B285EF71FD7EFB1D01
+#<|IYWXZFTJQ|>
+
+#hxxp[:]//serviceares[.]hopto[.]org/contig.txt
+#automovilespalma[.]golffan[.]us 6820
